a33905ebf32d53281191cdf185b2c32aa036a7c7
Arman Alimov <527c@mail.ru>
2022-05-02 16:18:47 +0600

    create Context

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -2,11 +2,13 @@ import React, {useState} from 'react';
 import {client, urlFor} from "../../lib/client";
 import {AiOutlineMinus, AiOutlinePlus, AiFillStar, AiOutlineStar} from "react-icons/ai";
 import {Product} from "../../components";
+import {useStateContext} from "../../context/StateContext";
 
 const ProductDetails = ({product, products}) => {
 
     const {image, name, details, price} = product
     const [index, setIndex] = useState(0)
+    const {incQuantity, decQuantity, quantity, onAddToCart} = useStateContext()
 
     return (
         <div>
@@ -43,19 +45,19 @@ const ProductDetails = ({product, products}) => {
                     <div className="quantity">
                         <h3>Quantity:</h3>
                         <p className="quantity-desc">
-                            <span className="minus" onClick="">
+                            <span className="minus" onClick={decQuantity}>
                                 <AiOutlineMinus/>
                             </span>
-                            <span className="num" onClick="">
-                                0
+                            <span className="num">
+                                {quantity}
                             </span>
-                            <span className="plus" onClick="">
+                            <span className="plus" onClick={incQuantity}>
                                 <AiOutlinePlus/>
                             </span>
                         </p>
                     </div>
                     <div className="buttons">
-                        <button type="button" className="add-to-cart" onClick="">Add to Cart</button>
+                        <button type="button" className="add-to-cart" onClick={() => onAddToCart(product, quantity)}>Add to Cart</button>
                         <button type="button" className="buy-now" onClick="">Buy Now</button>
                     </div>
                 </div>
@@ -64,7 +66,7 @@ const ProductDetails = ({product, products}) => {
                 <h2>You may also like</h2>
                 <div className="marquee">
                     <div className="maylike-products-container track">
-                        {products.map( item => <Product key={item._id} product={item}/>)}
+                        {products.map(item => <Product key={item._id} product={item}/>)}
                     </div>
                 </div>
             </div>
